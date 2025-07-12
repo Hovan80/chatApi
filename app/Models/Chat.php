c26693209b650a23f38ad3432a8f9493053035dc
ilya_frolov_04@bk.ru <ilya_frolov_04@bk.ru>
2025-07-12 11:40:01 +0300

    добавлены связи с другими моделями

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Chat extends Model
 {
-    //
+    protected $table = "chats";
+    protected $fillable = ["title, chat_type, chat_group_id"];
+
+    public function chatGroup(): BelongsTo
+    {
+        return $this->belongsTo(ChatGroup::class);
+    }
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function members(): BelongsToMany{
+        return $this->belongsToMany(User::class);
+    }
+
+    public function attachments(): hasMany{
+        return $this->hasMany(Attachment::class);
+    }
 }
